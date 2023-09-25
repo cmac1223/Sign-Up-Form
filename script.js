2e3fca7ef67398b658ac5bafdda3ff5b5ab9adcc
Chris McClain <chrismac539@gmail.com>
2023-09-24 23:09:23 -0400

    added javascript in order to validate matching passwords

---
+++ script.js
@@ -0,0 +1,21 @@
+// Validate form password using javascript
+// var password = document.getElementById('#password').value;
+// var confirmPassword = document.getElementById('#password_confrim').value;
+// console.log(password)
+// const button = document.querySelector('button');
+
+// button.addEventListener("click", (event) =>{
+
+// })
+
+function matchPassword() {
+  debugger;
+  var pw1 = document.getElementById('password');
+  var pw2 = document.getElementById('password_confirm');
+  if(pw1.value != pw2.value){
+    alert('Passwords did not match');
+  } else {
+    alert('Password created successfully');
+  }
+}
+
